96e185c8d4214fc8f75fb5827889052218cc447e
sahil <stsahiltaneja@gmail.com>
2023-01-22 16:35:45 +0530

    version 1.0 leetcode crawler

--- background.js
+++ background.js
@@ -1,13 +1,15 @@
 let leetcodeData = {
         submissionURL: "https://leetcode.com/api/submissions",
-        
+        garphql: "https://leetcode.com/graphql/",
+        problemQuery: {"query":" query questionTitle($titleSlug: String!) {question(titleSlug: $titleSlug) {questionId questionFrontendId title titleSlug    isPaidOnly    difficulty    likes    dislikes  }}    ","variables":{"titleSlug":"longest-palindromic-substring"}},
+        tagsQuery: {"query":"\n    query singleQuestionTopicTags($titleSlug: String!) {\n  question(titleSlug: $titleSlug) {\n    topicTags {\n      name\n      slug\n    }\n  }\n}\n    ","variables":{"titleSlug":"longest-palindromic-substring"}}
 }
 
 let configData ={
     host: "http://localhost:8080",
-    problemExists: "/problem/exists",
-    insertProblem: "/problem",
-    insertStats:"/userstats"
+    problemExists: "/injest/problem/exists",
+    insertProblem: "/injest/problem",
+    insertStats:"/injest/userstats/"
 
 }
 
@@ -33,10 +35,11 @@ async function main(sendResponse) {
             console.log(submissions)
             console.log(has_next)
             
-            let problemList = await getProblemList(submissions.submissions_dump)
-            //let problemsToInsert = await checkProblemExists(sendResponse, problemList)
-            //insertProblems(sendResponse, problemsToInsert)
-            // insertStats(sendResponse, submissions)
+            let problemList = getProblemList(submissions.submissions_dump)
+            let problemsToInsert = await checkProblemExists(problemList)
+            console.log(problemsToInsert)
+            insertProblems(sendResponse, problemsToInsert)
+            insertStats(sendResponse, submissions.submissions_dump, has_next)
         }
         catch(err){
             console.error(err)
@@ -66,26 +69,17 @@ async function getsubmissions(offset, limit, last){
 
 }
 
-async function getProblemList(submissions){
+function getProblemList(submissions){
     //get unique problems from the submissions list
     try{
+        console.log(submissions)
         let questions = new Set()
-        submissions.array.forEach(element => {
-            questions.add(submissions.title)
+        submissions.forEach(element => {
+            questions.add(element.title_slug)
         });
         console.log(questions)
 
-        let url = new URL(configData.host+configData.problemExists)
-        fetch(url,{
-            method: "POST",
-            credentials: "include",
-            body: questions,
-            headers: {
-                'Content-Type': 'application/json',
-              }
-
-        })
-
+        return questions
     }
     catch(err){
         console.error(err)
@@ -94,15 +88,160 @@ async function getProblemList(submissions){
 
 }
 
-async function checkProblemExists(sendResponse, problemList){
+async function checkProblemExists(problemList){        
+        try{
+            let url = new URL(configData.host+configData.problemExists)
+            let response = await fetch(url, {
+                method: "POST",
+                credentials: "include",
+                body: JSON.stringify([...problemList]),
+                headers: {
+                    
+                    'Content-Type': 'application/json',  
+                }
+            })
+            if(!response.ok){ throw new Error("some Error occured")}
+            let result = await response.json()
+            return result
+        }
+        catch(err){
+            console.log("some error occured")
+            return null
+        }
+    
 
 }
 
 async function insertProblems(sendResponse, problemsToInsert){
+    
+    let problems = []
+    
+    for(let i=0; i<problemsToInsert.length; i++){
+        question = getLeetcodeProblem(problemsToInsert[i])
+        tags = getTopicTags(problemsToInsert[i])
+        question = await question
+        tags = await tags
+        let tagNames = tags.map((t) => t.name)
+        let request = {
+            questionId: question.questionFrontendId,
+            title:question.title,
+            difficulty: question.difficulty,
+            isPaidOnly: question.isPaidOnly,
+            leetcode_name: question.titleSlug,
+            tags: tagNames
+        }
+        problems.push(request)
+    }
+    // insert the problems
+    let url = new URL(configData.host+configData.insertProblem)
+    try{
+        let response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(problems),
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json',
+            }
+
+        })
+        if(!response.ok) throw new Error("not able to insert the problems")
+        else{ console.log("problems inserted successfully")}
+    }
+    catch(err){
+        console.log(err)
+        console.log("unable to insert problems")
+    }       
 
 }
+async function getLeetcodeProblem(problem){
+    try{
+        let url = leetcodeData.garphql
+        let query = leetcodeData.problemQuery
+        query.variables.titleSlug = problem
 
-async function insertStats(sendResponse, submissions){
+        let response = await fetch(url, {
+            method:"POST",
+            body: JSON.stringify(query),
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+              }
+        })
+        if(!response.ok) throw new Error("Error occured while fetching leetcode")
+
+        let data = await response.json()
+        return data.data.question
+
+    }
+    catch(err){
+        console.log("unable to get the problem from leetcode")
+        return null
+    }
+}
+
+async function getTopicTags(problem){
+    try{
+        let url = leetcodeData.garphql
+        let query = leetcodeData.tagsQuery
+        query.variables.titleSlug = problem
+
+        let response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(query),
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json',
+              }
+
+        })
+        if(!response.ok) throw new Error("Error occured while fetching leetcode")
+        let data = await response.json()
+        return data.data.question.topicTags
+    }
+    catch(err){
+        console.log("unable to get the problem from leetcode")
+        return null
+    }
+}
+
+async function insertStats(sendResponse, submissions, has_next){
+    let url = new URL(configData.host+configData.insertStats)
+    url.searchParams.append("last", has_next)
+
+    let request = []
+    for(let i=0; i<submissions.length; i++){
+        let sub = submissions[i]
+        //TODO notes are differrnt here not the code notes have to get the different query for that
+        // for now setting the notes to false
+        let data = {
+            code:sub.code,
+        status: sub.status_display,
+        memory: sub.memory,
+        language: sub.lang,
+        timestamp: sub.timestamp,
+        runtime: sub.runtime,
+        questionTitle: sub.title,
+        isNotesPresent: false,
+        notes: ""
+        }
+        request.push(data)
+    }
+    try{
+        let response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(request),
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        if(!response.ok) throw new Error("unable to send the stats")
+
+    }
+    catch(err){
+        console.log(err)
+        console.log("unable to insert the stats")
+    }
 
 }
 
--- popup.js
+++ popup.js
@@ -1,4 +1,7 @@
 var button = document.getElementById("sync_button")
+var syncContainer = document.getElementsByClassName("sync").item(0)
+var loginContainer = document.getElementsByClassName("login").item(0)
+
 const [tab] = await chrome.tabs.query({currentWindow: true, active: true});
 var current_URL = tab.url
 console.log("current_url :" + current_URL)
@@ -9,7 +12,8 @@ if(!(/^https:\/\/leetcode\.com.*/).test(current_URL)){
 
 let configData ={
     host: "http://localhost:8080",
-    user: "/leetstats/user/me"
+    user: "/leetstats/user/me",
+    login: "/leetstats/auth/login"
 
 }
 
@@ -32,21 +36,70 @@ button.addEventListener("click", () => startSync().then(() => console.log("sync 
 async function checkLogin(){
     let username = await getUsername()
     if(username == null){
-        button.style.display="none"
-        // load the username and password page
+        syncContainer.classList.add("hide")
+        loginContainer.classList.remove("hide")
+    }
+    else{
+        syncContainer.classList.remove("hide")
+        loginContainer.classList.add("hide")
     }
     
 }
 
 async function getUsername(){
 
-    let url = new URL(configData.host+configData.user)
-    let response = await fetch(url)
-    if(!response.ok){
+    try{
+        let url = new URL(configData.host+configData.user)
+        let response = await fetch(url, {
+            credentials: "include"
+        })
+        if(!response.ok){
+            return null
+        }
+        let username = await response.text()
+        return username
+    }
+    catch(err){
         return null
     }
-    let username = await response.text()
-    return username
+}
+
+
+var loginButton = document.getElementById("login_button")
+loginButton.addEventListener("click", (event) => login(event).then(() => console.log("login process completed")))
+
+async function login(event){
+    event.preventDefault()
+    
+    let username = document.getElementById("username").value
+    let password = document.getElementById("password").value
+
+    let request = {"email": username, "password": password}
+    try{
+        let response = await fetch(configData.host+configData.login, {
+            method: "POST",
+            body: JSON.stringify(request),
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        if(!response.ok){
+            throw new Error("username or password is inavlid")
+        }
+
+        else{
+            syncContainer.classList.remove("hide")
+            loginContainer.classList.add("hide")
+        }
+
+    }
+    catch(err){
+        console.log("some error occured")
+        //handle the error case
+    }
+
+
 }
 
 checkLogin().then(()=>console.log("dasjlhdlahosfhoigbehgfi"))
